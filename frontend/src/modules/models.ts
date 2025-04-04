ebc04c7b8c88ab50dfad6d454ca513518f74c67d
Susanne D. <Susanne.dahesh@gritacademy.se>
2025-04-04 14:42:33 +0200

    Update models.ts

--- frontend/src/modules/models.ts
+++ frontend/src/modules/models.ts
@@ -1,17 +1,34 @@
-export interface Task {
-    title: string;
-    description: string;
-    role: string;
-    assigned: string;
-    assignedName: string;
-    status: "new" | "inProgress" | "done";
-    timestamp: number;
-    key?: string;
+export class TaskClass {
+  title: string;
+  description: string;
+  role: string;
+  assigned: string;
+  assignedName: string;
+  status: "new" | "inProgress" | "done";
+  timestamp: number;
+  key?: string;
+
+  constructor(
+    title: string,
+    description: string,
+    role: string,
+    assigned: string,
+    assignedName: string
+  ) {
+    this.title = title;
+    this.description = description;
+    this.role = role;
+    this.assigned = assigned;
+    this.assignedName = assignedName;
+    this.status = "new";
+    this.timestamp = Date.now();
   }
-  
-  export interface Member {
-    name: string;
-    role: string;
-    key?: string;
+
+  markDone() {
+    this.status = "done";
   }
-  
+
+  startTask() {
+    this.status = "inProgress";
+  }
+}
